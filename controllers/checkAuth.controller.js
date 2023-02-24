5a6ba5f7da7c5b1c6aa000f9c6761d62af6d3571
dongquanthaito <dongquanthaito@gmail.com>
2023-02-24 19:05:01 +0800

    .

--- controllers/checkAuth.controller.js
+++ controllers/checkAuth.controller.js
@@ -1,31 +1,12 @@
 module.exports = {
     checkAuth: async(req, res) => {
         try {
-            let decode = jwt.verify(req.headers.authorization,"abcxyz")
-            if(decode){
-                let account = await accountmodel.findById(decode._id)
-                if(account==null){
-                    res.json({
-                        statusCode: 498,
-                        valid:false,
-                        mess:"Token invalid"
-                    })
-                }else{
-                    req.account = account
-                    res.json('pong')
-                }
-            }else{
-                res.json({
-                    statusCode: 403,
-                    valid:false,
-                    mess:"Forbidden"
-                })
-            }
+            res.json('pong')
         } catch (error) {
             res.json({
-                statusCode: 403,
-                err: 'Token Error',
-                detail: error
+                code: 502,
+                mess: "Bad Gateway",
+                err: error
             })
         }
     }
