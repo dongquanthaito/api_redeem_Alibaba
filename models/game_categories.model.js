57f847fcb8b80358262ea82b7b97026e81e23c06
dongquanthaito <dongquanthaito@gmail.com>
2023-03-08 02:22:08 +0800

    .

--- models/game_categories.model.js
+++ models/game_categories.model.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 const game_categories_schema = mongoose.Schema({
     game_categories_id: {
         type: String,
-        required: true,
-        unique: true
+        required: true
     },
+    site: String,
     game_categories: {
         type: Array,
         required: true
