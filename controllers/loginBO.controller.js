60bf8d7461f8d7a5b31aa91a75ebc6d1ea82bbef
dongquanthaito <dongquanthaito@gmail.com>
2023-03-09 17:28:22 +0800

    .

--- controllers/loginBO.controller.js
+++ controllers/loginBO.controller.js
@@ -118,6 +118,63 @@ module.exports = {
         });
     },
 
+    loginBOshbet: () => {
+        let data = {
+            "account": information.shbet.usernameBO,
+            "password": information.shbet.passBO
+        };
+        
+        let config = {
+          method: 'post',
+          url: 'https://management.cdn-dysxb.com/api/2.0/account/login',
+          headers: { 
+            'accept': ' */*', 
+            'content-type': ' application/json', 
+            'origin': ' '+information.shbet.linkBO, 
+            'referer': ' '+information.shbet.linkBO+'/', 
+            'x-requested-with': ' XMLHttpRequest'
+          },
+          data : data
+        };
+        
+        return axios(config)
+        .then(async function (response) {
+            console.log("Login BO - Success")
+            let decode = jwt_decode(response.data.Result.AccessToken)
+            if(decode) {
+                let dataToken = {
+                    "exp": decode.exp,
+                    "nbf": decode.nbf,
+                    "Id": decode.Id,
+                    "Account": decode.Account,
+                    "IsSub": decode.IsSub,
+                    "IsVisible": decode.IsVisible,
+                    "Isvalidated": decode.Isvalidated,
+                    "Token": response.data.Result.AccessToken
+                }
+
+                try {
+                    let updateDataToken = await tokenBOModel.findOneAndUpdate({Account: decode.Account}, dataToken, {new: true})
+                    if(updateDataToken) {
+                        console.log("Find And Update SHBET Token into Schema - Success")
+                        console.log('-------------------')
+                    } else {
+                        await tokenBOModel.create(dataToken)
+                        console.log("Create SHBET Token into Schema - Success")
+                        console.log('-------------------')
+                    }
+                    
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+            return response.data.Result.AccessToken
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+
     getTokenBO: async(req, res) => {
         let {...query} = req.query
         console.log(query.site)
