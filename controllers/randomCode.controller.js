f00b4b97ad138ff777cfc42d8dbae45cb3e113c6
dongquanthaito <dongquanthaito@gmail.com>
2023-03-10 13:47:24 +0800

    .

--- controllers/randomCode.controller.js
+++ controllers/randomCode.controller.js
@@ -29,6 +29,7 @@ module.exports = {
         let exp_code = query.exp_code
         let round = query.round
         let promo_id = query.promo_id
+        let system = query.system
         
         for(i = 0; i < list_code_length; i++) {
             codeBox.push({
@@ -42,7 +43,8 @@ module.exports = {
               exp_code: exp_code,
               used_time: 0,
               round: round,
-              promo_id: promo_id
+              promo_id: promo_id,
+              system: system
             })
         }
 
