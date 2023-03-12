ccdb9a602b5544bacba08e61847dace7e8508fb3
dongquanthaito <dongquanthaito@gmail.com>
2023-03-12 21:10:23 +0700

    .

--- server.js
+++ server.js
@@ -1,30 +1,28 @@
-const express = require('express')
-const connectDb = require('./config/database')
-const bodyParser = require('body-parser')
-const Router = require('./routers')
-const app = express()
-const cors = require('cors')
-const Fingerprint = require('express-fingerprint')
+const express = require("express");
+const connectDb = require("./config/database");
+const bodyParser = require("body-parser");
+const Router = require("./routers");
+const app = express();
+const cors = require("cors");
+const Fingerprint = require("express-fingerprint");
 
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 
-app.use(bodyParser.json({ limit: '50mb' }))
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-app.use(cors())
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(cors());
 
-app.use(Fingerprint({
-    parameters:[
-        Fingerprint.useragent,
-        Fingerprint.acceptHeaders,
-        Fingerprint.geoip,
-    ]
-}))
+app.use(
+  Fingerprint({
+    parameters: [Fingerprint.useragent, Fingerprint.acceptHeaders, Fingerprint.geoip],
+  })
+);
 
-connectDb()
-Router(app)
+connectDb();
+Router(app);
 
-app.get('/test',(req,res,next)=>{
-    res.send("Test được rồi nè")
-})
+app.get("/test", (req, res, next) => {
+  res.send("Test được rồi nè");
+});
 
-app.listen('5005', ()=> console.log("Server working in port 5005"))
+app.listen("5005", () => console.log("Server working in port 5005"));
